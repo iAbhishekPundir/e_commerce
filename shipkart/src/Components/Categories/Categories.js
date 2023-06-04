42657dbc35c36404169fef48439588826b227660
Abhishek <pundirabhishek987@gmail.com>
2023-06-04 20:25:43 +0530

    refactor: code

--- shipkart/src/Components/Categories/Categories.js
+++ shipkart/src/Components/Categories/Categories.js
@@ -4,12 +4,13 @@ import { SingleCategory } from "./SingleCategory";
 import { DataState } from "../../Contexts/Data/DataContext";
 
 
+
 export const Categories = () => {
   const { state: { categories }} = DataState();
 
   return (
     <>
-      <p className="category-header">Shop by categories</p>
+      <p className="category-header">SHOP BY CATEGORIES 👇</p>
       <div className="categories">
         {categories?.map((category) => (
           <SingleCategory category={category} key={category._id} />
