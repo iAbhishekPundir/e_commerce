5f7572b2b85162f3a7655baf38370a604d595111
Abhishek <pundirabhishek987@gmail.com>
2023-06-04 19:32:04 +0530

    feature: added footer Component

--- shipkart/src/Pages/Home/Home.jsx
+++ shipkart/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from '../../Components/Header/Header'
 import { Categories } from '../../Components/Categories/Categories'
+import { Footer } from '../../Components/Footer/Footer'
 
 export const Home = () => {
   return (
     <>
         <Header/>
         <Categories />
+        <Footer />
     </>
   )
 }
