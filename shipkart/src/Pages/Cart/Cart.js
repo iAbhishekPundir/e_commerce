10274c60d910f7f3daaa30d85e7326be9175f9ba
Abhishek <pundirabhishek987@gmail.com>
2023-06-06 22:38:14 +0530

    feature: Cart Component

--- shipkart/src/Pages/Cart/Cart.js
+++ shipkart/src/Pages/Cart/Cart.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "./Cart.css";
+import { SingleCartProduct } from "./Components/SingleCartProduct/SingleCartProduct";
+import { PriceDetails } from "./Components/CartPriceDetails/PriceDetails";
+import { DataState } from "../../Contexts/Data/DataContext";
+// import empty_cart from "../../Assets/empty_cart.svg";
 
 export const Cart = () => {
+  const {
+    state: { cart },
+  } = DataState();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div>Cart</div>
-  )
-}
+    <>
+      {cart?.length > 0 ? (
+        <div>
+          <h2 className="text-center top-margin">My Cart({cart?.length})</h2>
+          <div className="cart-main">
+            <div className="cart-container">
+              {cart?.map((product) => (
+                <SingleCartProduct key={product._id} product={product} />
+              ))}
+            </div>
+            <PriceDetails />
+          </div>
+        </div>
+      ) : (
+        <div className="text-center">
+          {/* <img src={empty_cart} alt="empty_cart" height={250} width={250} /> */}
+          <h2 className="text-center top-margin">Your Cart is Empty ☹️</h2>
+        </div>
+      )}
+    </>
+  );
+};
