82b64b018318e76a3dc5d733965ac78e65860bf3
Abhishek <pundirabhishek987@gmail.com>
2023-06-06 23:46:44 +0530

    feature: Require Auth

--- shipkart/src/App.js
+++ shipkart/src/App.js
@@ -1,27 +1,94 @@
 import "./App.css";
 import "./utils.css";
-import React from "react";
-import Mockman from "mockman-js";
-import { Routes, Route} from "react-router-dom"
+import { injectStyle } from "react-toastify/dist/inject-style";
 import { Home } from "./Pages/Home/Home";
-import { Navbar } from "./Components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
 import { ProductListing } from "./Pages/ProductListing/ProductListing";
-import { Wishlist } from "./Pages/Wishlist/Wishlist";
+import { Navbar } from "./Components/Navbar/Navbar";
 import { Cart } from "./Pages/Cart/Cart";
+import { Wishlist } from "./Pages/Wishlist/Wishlist";
+import { ProductDetail } from "./Pages/ProductDetail/ProductDetail";
+import Mockman from "mockman-js";
+import { RequiresAuth } from "./Components/RequiresAuth/RequiresAuth";
+import { Login } from "./Pages/Login/Login";
+import { SignUp } from "./Pages/Login/SignUp";
 import { Profile } from "./Pages/Profile/Profile";
+import { Checkout } from "./Pages/Checkout/Checkout";
+import { ToastContainer } from "react-toastify";
+import { DataState } from "./Contexts/Data/DataContext";
+import { Loading } from "./Components/Loading/Loading";
+import { PageNotFound } from "./Pages/ErrorPage/PageNotFound";
+import { OrderPlaced } from "./Pages/Order_Placed/OrderPlaced";
 
 function App() {
+  injectStyle();
+  const { loading } = DataState();
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/productlisting" element={<ProductListing/> }/>
-        <Route path="/wishlist" element={<Wishlist/> }/>
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/mockman" element={<Mockman />} />
-      </Routes>
+      {loading && <Loading />}
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/productlisting" element={<ProductListing />} />
+          <Route
+            path="/cart"
+            element={
+              <RequiresAuth>
+                <Cart />
+              </RequiresAuth>
+            }
+          />
+          <Route
+            path="/wishlist"
+            element={
+              <RequiresAuth>
+                <Wishlist />
+              </RequiresAuth>
+            }
+          />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/profile"
+            element={
+              <RequiresAuth>
+                <Profile />
+              </RequiresAuth>
+            }
+          />
+          <Route path="/mockman" element={<Mockman />} />
+          <Route
+            path="/checkout"
+            element={
+              <RequiresAuth>
+                <Checkout />
+              </RequiresAuth>
+            }
+          />
+          <Route
+            path="/orderPlaced"
+            element={
+              <RequiresAuth>
+                <OrderPlaced />
+              </RequiresAuth>
+            }
+          />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+        <ToastContainer
+          position="top-center"
+          autoClose={1000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </div>
     </div>
   );
 }
